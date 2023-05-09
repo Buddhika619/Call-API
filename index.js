4fd7bcf868705629bfae3a0a9b011962386d2a4e
Buddhika Gamage <49301867+Buddhika619@users.noreply.github.com>
2023-05-09 10:07:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const app = express();
 // var apn = require("apn");
 const { v4: uuidv4 } = require("uuid");
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
+
+const messaging = admin.messaging();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +22,81 @@ app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/initiate-call",async (req, res) => {
+  console.log(req.body)
+  
+  const { calleeInfo, callerInfo } = req.body;
+
+  if (calleeInfo.platform === "iOS") {
+    
+   
+  } else if (calleeInfo.platform === "ANDROID") {
+    var FCMtoken = calleeInfo.token;
+    const info = JSON.stringify({
+      callerInfo,
+     
+      type: "CALL_INITIATED",
+    });
+    var message = {
+      data: {
+        info,
+      },
+      android: {
+        priority: "high",
+      },
+      token: FCMtoken,
+    };
+
+  await messaging.send(message)
+  .then((response) => {
+    console.log('Successfully sent message:', response);
+  })
+  .catch((error) => {
+    console.log('Error sending message:', error);
+  });
+  } else {
+    res.status(400).send("Not supported platform");
+  }
+  res.send('done')
+});
+
+app.post("/update-call",async (req, res) => {
+  console.log(req.body)
+  
+  const { callerInfo, type } = req.body;
+  const info = JSON.stringify({
+    callerInfo,
+    type,
+  });
+  console.log(type)
+  var message = {
+    data: {
+      info,
+    },
+    apns: {
+      headers: {
+        "apns-priority": "10",
+      },
+      payload: {
+        aps: {
+          badge: 1,
+        },
+      },
+    },
+    token: callerInfo.token,
+  };
+
+ await messaging.send(message)
+  .then((response) => {
+    console.log('Successfully sent message:', response);
+  })
+  .catch((error) => {
+    console.log('Error sending message:', error);
+  });
+
+  res.send('done')
 });
 
 
